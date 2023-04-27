5d90bef0745aaee38347e7034d84cd93a24e39f9
Nishant Kumar <nishants440@gmail.com>
2023-04-27 22:08:06 +0530

    Connecting Backend to Frontend

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,53 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { firestore } from "src/server/config";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  doc,
+  updateDoc,
+  query,
+  where,
+  setDoc,
+  deleteDoc,
+  orderBy,
+  serverTimestamp,
+} from "firebase/firestore";
 
-type Data = {
-  name: string
+let sampleRef = collection(firestore, "users");
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  imageLink: string;
+  userID: string;
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<User[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  onSnapshot(
+    sampleRef,
+    (response) => {
+      const users: User[] = [];
+      response.forEach((doc) => {
+        const data = doc.data();
+        users.push({
+          id: doc.id,
+          name: data.name,
+          email: data.email,
+          imageLink: data.imageLink,
+          userID: data.userID,
+        });
+      });
+
+      res.status(200).json(users);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
 }
